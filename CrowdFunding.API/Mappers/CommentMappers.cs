473655aa69220522b01eec1c24083fc1204749ae
Antoine Paquay <antoinepaquay64@gmail.com>
2022-07-30 00:59:01 +0200

    Added CommentMappers & CommentForm

---
+++ CrowdFunding.API/Mappers/CommentMappers.cs
@@ -0,0 +1,30 @@
+﻿using CrowdFunding.API.Models;
+
+namespace CrowdFunding.API.Mappers
+{
+    public static class CommentMappers
+    {
+        public static Comment ToModel(this BLL.Entities.CommentEntity e)
+        {
+            return new Comment
+            {
+                Id = e.Id,
+                MemberId = e.MemberId,
+                ProjectId = e.ProjectId,
+                Created = e.Created,
+                LastModified = e.LastModified,
+                Text = e.Text
+            };
+        }
+
+        public static BLL.Entities.CommentEntity ToBLL(this CommentForm e)
+        {
+            return new BLL.Entities.CommentEntity
+            {
+                Text = e.Text,
+                MemberId = e.MemberId,
+                ProjectId = e.ProjectId,
+            };
+        }
+    }
+}
--- CrowdFunding.API/Models/Comment.cs
+++ CrowdFunding.API/Models/Comment.cs
@@ -9,4 +9,11 @@
         public DateTime Created { get; set; }
         public DateTime? LastModified { get; set; }
     }
+
+    public class CommentForm
+    {
+        public string? Text { get; set; }
+        public int MemberId { get; set; }
+        public int ProjectId { get; set; }
+    }
 }
