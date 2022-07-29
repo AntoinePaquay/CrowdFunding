f9ead78991b5281071293611f233618ef4044513
Antoine Paquay <antoinepaquay64@gmail.com>
2022-07-30 01:41:23 +0200

    Added Member mappers

---
+++ CrowdFunding.API/Mappers/MemberMappers.cs
@@ -0,0 +1,38 @@
+﻿using CrowdFunding.API.Models;
+
+namespace CrowdFunding.API.Mappers
+{
+    public static class MemberMappers
+    {
+        public static Member ToModel(this BLL.Entities.MemberEntity e)
+        {
+            return new Member
+            {
+                Id = e.Id,
+                Image = e.Image,
+                BirthDate = e.BirthDate,
+                Created = e.Created,
+                Email = e.Email,
+                FirstName = e.FirstName,
+                LastModified = e.LastModified,
+                LastName = e.LastName,
+                PasswordHash = e.PasswordHash,
+                Username = e.Username
+            };
+        }
+
+        public static BLL.Entities.MemberEntity ToBLL(this MemberForm e)
+        {
+            return new BLL.Entities.MemberEntity
+            {
+                Image = e.Image,
+                BirthDate = e.BirthDate,
+                Email = e.Email,
+                FirstName = e.FirstName,
+                LastName = e.LastName,
+                PasswordHash = e.PasswordHash,
+                Username = e.Username
+            };
+        }
+    }
+}
