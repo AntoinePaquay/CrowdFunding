665abccd6578b404f9ad77369f8018fa8833a489
Antoine Paquay <antoinepaquay64@gmail.com>
2022-07-30 01:33:46 +0200

    Added Member model and form

---
+++ CrowdFunding.API/Models/Member.cs
@@ -0,0 +1,27 @@
+﻿namespace CrowdFunding.API.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string? Image { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+
+    public class MemberForm
+    {
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string? Image { get; set; }
+    }
+}
